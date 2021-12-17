2ee5ec3da4765204475d26bdbdd30e37a36c934e
Magesh Kuppan <tkmagesh77@gmail.com>
2021-12-17 14:24:17 +0530

    solution ver 3.0

--- 01-Language/src/calc.js
+++ 01-Language/src/calc.js
@@ -17,6 +17,8 @@ function add(){
 } 
 */
 
+//ver 2.0
+/* 
 function add(){
     function parseArg(n){
         if (typeof n === 'function') return parseArg(n());
@@ -27,4 +29,15 @@ function add(){
     for (var i = 0; i < arguments.length; i++)
         result += parseArg(arguments[i])
     return result
+} 
+*/
+
+//ver 3.0
+function add(){
+    function parseArg(n){
+        if (typeof n === 'function') return parseArg(n());
+        if (Array.isArray(n)) return add.apply(this, n);
+        return isNaN(n) ? 0 : parseInt(n);
+    }
+    return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add([].slice.call(arguments,1));
 }
