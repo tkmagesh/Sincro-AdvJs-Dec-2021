4486fe67a0c3571ea981a7e5c6b9b464d1d60c3f
Magesh Kuppan <tkmagesh77@gmail.com>
2021-12-17 12:59:59 +0530

    add function - solution - v1.0 & v2.0

--- 01-Language/src/calc.js
+++ 01-Language/src/calc.js
@@ -1,3 +1,4 @@
+/* 
 function add(){
     function parseArg(n){
         if (typeof n === 'function') return parseArg(n());
@@ -7,6 +8,19 @@ function add(){
                 result += parseArg(n[i])
             return result
         }
+        return isNaN(n) ? 0 : parseInt(n);
+    }
+    var result = 0;
+    for (var i = 0; i < arguments.length; i++)
+        result += parseArg(arguments[i])
+    return result
+} 
+*/
+
+function add(){
+    function parseArg(n){
+        if (typeof n === 'function') return parseArg(n());
+        if (Array.isArray(n)) return add.apply(this, n);
         return isNaN(n) ? 0 : parseInt(n);
     }
     var result = 0;
