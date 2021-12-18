883af5ac6b7facacd160d154befbe5e39f6b9ee8
Magesh Kuppan <tkmagesh77@gmail.com>
2021-12-18 17:13:20 +0530

    Day-02 complete

--- 01-Language/src/calc.js
+++ 01-Language/src/calc.js
@@ -33,6 +33,7 @@ function add(){
 */
 
 //ver 3.0
+/* 
 function add(){
     function parseArg(n){
         if (typeof n === 'function') return parseArg(n());
@@ -40,4 +41,27 @@ function add(){
         return isNaN(n) ? 0 : parseInt(n);
     }
     return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add([].slice.call(arguments,1));
+} 
+*/
+
+//ver 4.0
+/* 
+function add(){
+    function parseArg(n){
+        if (typeof n === 'function') return parseArg(n());
+        if (Array.isArray(n)) return add.apply(this, n);
+        return isNaN(n) ? 0 : parseInt(n);
+    }
+    return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add(Array.prototype.slice.call(arguments,1));
+} 
+*/
+
+//ver 5.0
+function add(...args){
+    function parseArg(n){
+        if (typeof n === 'function') return parseArg(n());
+        if (Array.isArray(n)) return add(...n);
+        return isNaN(n) ? 0 : parseInt(n);
+    }
+    return args.length <= 1 ? parseArg(args[0]) : parseArg(args[0]) + add(args.slice(1));
 }
